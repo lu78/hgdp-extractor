3ee0fbf2d9c48b703c290ce23c3d90e01c808d58
dalloliogm <dalloliogm@gmail.com>
2008-12-15 18:34:08 +0100

    switched to optparser

--- src/hgdp_snp_extractor.py
+++ src/hgdp_snp_extractor.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # Copyright GPL 3.0 Giovanni Dall'Olio
 """
-Extract SNPs from a HGDP SNP file. 
+Extract SNPs from a HGDP SNP file.     #TODO: refactore everything!! 
 
 usage:
     python hgdp_snp_extractor.py 
@@ -27,56 +27,48 @@ def usage():
     sys.exit()
 
 def parameters():
-    # Read arguments and parameters
-    try:
-        opts, args = getopt.getopt(sys.argv[1:], 
-                                   "ht", ["help", "test", "samplefile=", "genotypes_by_chr_dir=",
-                                          "chromosomes=", "continent=", "outputfile="])
-
-    except getopt.GetoptError, err:
-        usage()
-
-    if opts == []:
-        usage()
-        
-    samplesfile = ''
-    genotypes_by_chr_dir = ''
-    selected_chr = []
-    continent = ''
-
-    for opt, arg in opts:
-        if opt in ('-h', '--help'):
-            usage()
-        elif opt in ('--test', '-t'):
-            _test()
-        elif opt in ('--samplesfile', '-s'):
-            samplesfile = arg
-        elif opt in ('--window', '-w'):
-            sliding_windows_file_path = arg
-        elif opt in ('--output', '-o'):
-            output_file_path = arg
-        
-
-    # TODO: getopt interface
+    """Read arguments and parameters"""
+    
     basedir = '/home/gioby/Data/HGDP/'
-    if samplesfile == '':    
-        samplesfile = file(basedir + 'Annotations/samples_subset.csv', 'r')
-    if genotypes_by_chr_dir == '':
-        genotypes_by_chr_dir = basedir + 'Genotypes_by_chr/'
-    if selected_chr == []:
-        selected_chr = [22, ]
-    if continent == '':
-        continent = 'Europe'
-        
-    genotypes_files = [genotypes_by_chr_dir + '/chr' + str(chrom) + '.geno' 
-                       for chrom in selected_chr]
-    logging.debug(genotypes_files)
+    
+    # parse parameters
+    parser = OptionParser()
+    
+    
+    parser.set_defaults(genotypes_by_chr_dir = basedir + 'Genotypes_by_chr/',
+                        samplesfilepath = basedir + 'Annotations/samples_subset.csv',
+                        selected_chr = [22, ],  #FIXME: only the first chromosome is used 
+                        continent = 'Europe')
+    
+    parser.add_option('-s', '--samplefile', action='store', type='string', 
+                      dest = 'samplesfilepath')
+    parser.add_option('-g', '--genotypes_by_chr_dir', action='store', 
+                      type='string', dest = 'genotypes_by_chr_dir')
+    parser.add_option('-c', '--continent', action='store', type='string', 
+                      dest = 'continent')
+    parser.add_option('-y', '--chromosomes', action='store', type='list', 
+                      dest = 'chromosomes')
+    parser.add_option('-o', '--outputfile', action='store', type='string', 
+                      dest = 'outputfile')
+    parser.add_option('-t', '--test', action='callback', dest = _test)
+    parser.add_option('-h', '--help', action='help')
+    
+    
+    (options, args) = parser.parse_args()
+#    if len(args) != 1:
+#        parser.error("incorrect number of arguments")        
+
+    samplesfile = file(options.samplesfilepath, 'r')
+    genotypes_files = [options.genotypes_by_chr_dir + '/chr' + str(chrom) + '.geno' 
+                       for chrom in options.selected_chr]
+    logging.debug(options.genotypes_files)
 #    continent = 'Asia'
-    outputfile = basedir + 'Results/hgdp_chr22_' + continent + '.geno'
+    outputfile = basedir + 'Results/hgdp_chr%s_%s.geno' %(options.chromosomes[0], 
+                                                          options.continent)
     
     # Read Samples File
     samples = hgdpsamplesfileParser(samplesfile)
-    samples_filter = [sample for sample in samples if sample.continent == continent]
+    samples_filter = [sample for sample in samples if sample.continent == options.continent]
     
     return samples_filter, genotypes_files, outputfile
 
