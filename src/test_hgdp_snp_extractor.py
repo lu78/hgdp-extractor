3d99925a352dbb4d1dd2ab1443cf7920c2b4aeb7
dalloliogm <dalloliogm@gmail.com>
2009-01-16 19:19:43 +0100

    improved unittest skeleton, added support from the command module

--- src/test_hgdp_snp_extractor.py
+++ src/test_hgdp_snp_extractor.py
@@ -1,17 +1,26 @@
 import unittest
+import commands
 
 class test_hgdpextractor(unittest.testcase):
     """ """
+    samplesfile = '/home/gioby/Data/HGDP/Annotations/samples_subset.csv'
+    
 
     def setUp(self):
         """
         launch the python script hgdp_snp_extractor
         """
-
+        python_command = 'python hgdp_snp_extractor -c %s -y %s -g %s' %(self.continent, self.chromosome, self.genotypes_by_chr_dir)
+        
+        self.output = commands.getoutput('python')
 
 
 class test_europen_chr1(test_hgdpextractor):
-    pass
 
+    def __init__(self):
+        self.continent = 'Europe'
+        self.chromosome = '1'
+        self.genotypes_by_chr_dir = '' #TODO: define here
+   
 
         
