aba2fc15805dc790e423190dc6046dc3df49f143
dalloliogm <dalloliogm@gmail.com>
2009-01-16 17:31:31 +0100

    Added an unittest

---
+++ src/test_hgdp_snp_extractor.py
@@ -0,0 +1,17 @@
+import unittest
+
+class test_hgdpextractor(unittest.testcase):
+    """ """
+
+    def setUp(self):
+        """
+        launch the python script hgdp_snp_extractor
+        """
+
+
+
+class test_europen_chr1(test_hgdpextractor):
+    pass
+
+
+        
