d7c0f03836c2b464dc0b9a74c85f8897404902c6
dalloliogm <dalloliogm@gmail.com>
2009-01-19 12:54:08 +0100

    refactored unittest (some variables moved outside to __init__, etc..)

--- src/test_hgdp_snp_extractor.py
+++ src/test_hgdp_snp_extractor.py
@@ -1,9 +1,19 @@
 import unittest
 import commands
+import difflib
+import filecmp
 
-class test_hgdpextractor(unittest.testcase):
+class test_hgdpextractor(unittest.TestCase):
     """ """
+    # The following variables should be common for all the tests:
     samplesfile = '/home/gioby/Data/HGDP/Annotations/samples_subset.csv'
+    genotypes_by_chr_dir = '/home/gioby/Data/HGDP/Genotypes_by_chr'
+    
+    # The following variables should be redefined in every subclassed test:
+    continent = ''
+    chromosome = ''
+    outputfile = ''
+    expected_outputfile = ''
     
 
     def setUp(self):
@@ -14,13 +24,23 @@ class test_hgdpextractor(unittest.testcase):
         
         self.output = commands.getoutput('python')
 
+    def test_outputfile_is_expected(self):
+        """Checks that the output is equal to the expected"""
+        out = self.outputfile
+        expected = self.expected_outputfile
+        
+        # basic comparison #TODO: use difflib module
+        filecmp.cmp(out, expected, shallow=False)
 
 class test_europen_chr1(test_hgdpextractor):
+    """test extraction of chromosome 1 from europeans"""
 
-    def __init__(self):
-        self.continent = 'Europe'
-        self.chromosome = '1'
-        self.genotypes_by_chr_dir = '' #TODO: define here
+    continent = 'Europe'
+    chromosome = '1'
+
+    outputfile = ''
+    expected_output = '/home/gioby/Data/HGDP/Test/chr1_30_filtered_europe.txt:'
+
    
 
         
